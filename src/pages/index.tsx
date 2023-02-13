06ace90ed92285120aa9ba202081477271841285
meljadyn <melody.d.duncan@gmail.com>
2023-02-13 15:08:27 -0500

    Add navbar to public landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import { Title } from '@mantine/core';
+import { getServerSession } from "next-auth/next";
+import type { GetServerSidePropsContext } from "next";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import LandingNavbar from "../components/navigation/LandingNavbar";
 
 function Home() {
   return (
@@ -7,9 +11,27 @@ function Home() {
       <Head>
         <title>Stepi</title>
       </Head>
-      <Title>Stepi</Title>
+      <LandingNavbar />
     </>
   );
+}
+
+// Redirect to /dashboard if user is logged in
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
 
 export default Home;
